64d190e8c7aea9eaf9f48b280f076273cf29a2a1
kamil <kamil.szczepanik00@gmail.com>
2021-03-31 18:27:51 +0200

    Adding initialized Spring-Boot project

---
+++ Clinic/src/test/java/com/thsgroup/Clinic/ClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thsgroup.Clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

68d1268b83af548a373a64485cd8594f6501bb57
kamil <kamil.szczepanik00@gmail.com>
2021-05-04 18:43:04 +0200

    Added Login and Registration + email verification

---
+++ Clinic/src/main/java/com/thsgroup/Clinic/registration/token/ConfirmationTokenService.java
@@ -0,0 +1,27 @@
+package com.thsgroup.Clinic.registration.token;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class ConfirmationTokenService {
+    
+    private final ConfirmationTokenRepository confirmationTokenRepository;
+
+    public void saveConfirmationToken(ConfirmationToken token) {
+        confirmationTokenRepository.save(token);
+    }
+
+    public Optional<ConfirmationToken> getToken(String token) {
+        return confirmationTokenRepository.findByToken(token);
+    }
+
+    public int setConfirmed(String token) {
+        return confirmationTokenRepository.updateConfirmedAt(token, LocalDateTime.now());
+    }
+}

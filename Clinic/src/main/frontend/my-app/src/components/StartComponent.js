fbef7847a1a5e3554abaf26b63ebebf2e9114ecb
Piotr Hondra <piotrhondra@gmail.com>
2021-05-17 13:56:12 +0200

    Added front page layout

--- Clinic/src/main/frontend/my-app/src/components/StartComponent.js
+++ Clinic/src/main/frontend/my-app/src/components/StartComponent.js
@@ -6,15 +6,28 @@ class StartComponent extends React.Component{
     
     render(){
         return(
-            <>
-            <h3>Żeby mieć dostęp do strony musisz być zalogowany</h3>
-            <NavLink to='/register' exact>Przejdź do rejestracji</NavLink>
-            <br></br>
-            <NavLink to='/login' exact>Przejdź do logowania</NavLink>
-
-            
-
-            </>  
+            <div className="container-fluid" id="front">
+                <div className="container py-5">
+                    <div className="bg-dark col-md-7 col-lg-6 m-auto rounded-3 pb-5 px-5 pt-2" id="jumbo">
+                <i className="fa fa-lock p-3" aria-hidden="true"></i>
+            <p>Żeby mieć dostęp do strony musisz być zalogowany</p>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <NavLink to='/register' exact><button className="btn btn-outline-light btn-sm-lg m-2">Rejestracja</button></NavLink>
+                    </div>
+                    <div class="col">
+                        <NavLink to='/login' exact><button className="btn btn-outline-light btn-sm-lg m-2">Logowanie</button></NavLink>
+                        
+                    </div>
+                    
+                </div>
+                
+            </div>
+            </div>
+            </div>
+            </div>
+             
         )
     }
 }
